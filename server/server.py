003186c5505682313c2bedc8633f510986593daa
dinglindong <258231674@qq.com>
2019-04-12 05:42:26 +0800

    add base server

---
+++ server/server.py
@@ -0,0 +1,8 @@
+from wsgiref.simple_server import make_server
+
+from hello import application
+
+httpd = make_server('',8000,application)
+print('Server Http on port 8000...')
+
+httpd.serve_forever()
